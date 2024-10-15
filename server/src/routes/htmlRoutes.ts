9c359824e27c0e97dc6d046097c0edcf26dd6b0b
flynno <luke.laz.flynn@gmail.com>
2024-10-14 21:40:58 -0600

    setup route handling for static HTML files and API endpoints

--- server/src/routes/htmlRoutes.ts
+++ server/src/routes/htmlRoutes.ts
@@ -5,6 +5,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const router = Router();
 
-// TODO: Define route to serve index.html
+// Serve up the index.html
+router.get('/', (_, res) => {
+  res.sendFile(path.join(__dirname, '../client/index.html'));
+});
 
 export default router;
