a5a2686b7119bd82e899a4c63221c25841461558
flynno <luke.laz.flynn@gmail.com>
2024-10-15 12:28:41 -0600

    fixed issue with build folder being ignored

---
+++ server/dist/routes/api/weatherRoutes.js
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+const router = Router();
+import HistoryService from '../../service/historyService.js';
+import WeatherService from '../../service/weatherService.js';
+// POST /weather -> get weather data using city name
+router.post('/', async (req, res) => {
+    // GET weather data using city name
+    const city = req.body.cityName;
+    const weatherData = await WeatherService.getWeatherForCity(city);
+    res.json(weatherData);
+    // Save city to search history
+    await HistoryService.addCity(city);
+});
+// GET /weather -> get search history
+router.get('/history', async (_, res) => {
+    const cities = await HistoryService.getCities();
+    res.json(cities);
+});
+// DELETE city from search history
+router.delete('/history/:id', async (req, res) => {
+    const id = req.params.id;
+    await HistoryService.removeCity(id);
+    res.sendStatus(204);
+});
+export default router;
