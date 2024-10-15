fdd258c56b6cecc66c9c2af235718c8aae77cc15
flynno <luke.laz.flynn@gmail.com>
2024-10-15 07:12:14 -0600

    updated types in historyService.js

--- server/src/service/historyService.ts
+++ server/src/service/historyService.ts
@@ -1,56 +1,18 @@
 import fs from 'node:fs/promises';
 
-interface ICity {
-  getName(): string;
-  getId(): string;
-  getLongitude(): number;
-  getLatitude(): number;
-  getCoordinates(): {longitude: number, latitude: number};
-}
+class City {
+  name: string;
+  id: string;
 
-class City implements ICity{
-  private name: string;
-  private id: string;
-  private longitude: number;
-  private latitude: number;
-
-  constructor (name: string, id: string, longitude: number, latitude: number) {
+  constructor(name: string, id: string) {
     this.name = name;
     this.id = id;
-    this.longitude = longitude;
-    this.latitude = latitude;
-  }
-  
-  // Get the city name
-  public getName() {
-    return this.name;
-  }
-
-  // Get the city ID
-  public getId() {
-    return this.id;
-  }
-
-  // Get the city's longitude
-  public getLongitude() {
-    return this.longitude;
-  }
-
-  // Get the city's latitude
-  public getLatitude() {
-    return this.latitude;
-  }
-
-  // Get the city's coordinates in an object
-  public getCoordinates() {
-    return {longitude: this.longitude, latitude: this.latitude};
   }
 }
 
-// TODO: Complete the HistoryService class
 class HistoryService {
   // Reads data from the searchHistory.json file
-  private async read(): Promise<City[]> {
+  private async read(): Promise<any[]> {
     try {
       const data = await fs.readFile('../../db/searchHistory.json', 'utf-8');
       return JSON.parse(data);
@@ -72,20 +34,20 @@ class HistoryService {
   // Reads the cities from the searchHistory.json file and returns them as an array of City objects
   async getCities(): Promise<City[]> {
     const cities = await this.read();
-    return cities.map((city: any) => new City(city.name, city.id, city.longitude, city.latitude));
+    return cities.map((city: any) => new City(city.name, city.id));
   }
 
   // Writes a new city object to the searchHistory.json file
-  async addCity(city: City): Promise<void> { // TODO: update parameters to accept City object parameters
+  async addCity(city: string, id: string): Promise<void> { // TODO: update calls to this method to pass in ID as well
     const cities = await this.read();
-    cities.push(city);
+    cities.push(new City(city, id));
     await this.write(cities);
   }
 
   // Removes a city from the searchHistory.json file based on the city ID
   async removeCity(id: string): Promise<void> {
     const cities = await this.read();
-    const newCities = cities.filter((city: City) => city.getId() !== id);
+    const newCities = cities.filter((city: any) => city.id !== id);
     await this.write(newCities);
   }
 }
